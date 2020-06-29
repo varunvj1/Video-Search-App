bd5909cffced7f6f5cae3450fc4c77bf1060d84f
Varun Jain <varunvj181@gmail.com>
2020-06-29 18:14:08 +0530

    Updates

---
+++ precache-manifest.3092a0119cabe027842a8353332dfe36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20fc58a8504a2f2b0dc8f62e62c69f59",
+    "url": "/Video-Search-App/index.html"
+  },
+  {
+    "revision": "08a56234e3cc5875af3b",
+    "url": "/Video-Search-App/static/css/main.cfa7b75b.chunk.css"
+  },
+  {
+    "revision": "f41fd6e7cea080df2932",
+    "url": "/Video-Search-App/static/js/2.f7a079da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Video-Search-App/static/js/2.f7a079da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08a56234e3cc5875af3b",
+    "url": "/Video-Search-App/static/js/main.595e0d47.chunk.js"
+  },
+  {
+    "revision": "5f2b41a79db9f2ab7849",
+    "url": "/Video-Search-App/static/js/runtime-main.897c6971.js"
+  }
+]);
